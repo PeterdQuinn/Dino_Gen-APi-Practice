803073a65797f9706014575e57904ad8c7670716
PeterdQuinn <75184837+PeterdQuinn@users.noreply.github.com>
2021-07-14 02:08:19 -0700

    changed index.js

--- index.js
+++ index.js
@@ -2,46 +2,30 @@ if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config();
 }
 
-const { request, response } = require('express');
 const express = require('express');
 const app = express();
-const port = 3000;
-const fetch = require('node-fetch')
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+/**catch all error handler */
+const { errorHandler } = require('./middleware/errorHandler.middleware');
+/**dino api routes */
+const dinoRouter = require('./api/dino/dino.routes')
+app.use('/api/dino', dinoRouter)
 
+// global error handler
+app.use(errorHandler);
 
+process
+	.on('unhandledRejection', (reason, p) => {
+		console.error(reason, 'Unhandled Rejection at Promise', p);
+	})
+	.on('uncaughtException', (err) => {
+		console.error(err, 'Uncaught Exception thrown');
+		process.exit(1);
+	});
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+	console.log(`Example app listening at http://localhost:${PORT}`);
 });
-
-
-
-app.get(`/dinoname`, async(request, response) =>{
-   const fetchApi = await fetch("https://alexnormand-dino-ipsum.p.rapidapi.com/?paragraphs=1&words=2&format=json", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": api_key,
-		"x-rapidapi-host": "alexnormand-dino-ipsum.p.rapidapi.com"
-    },
-});
-     const dinoNameResponse = await fetchApi.json();
-     console.log(dinoNameResponse);
-     response.json(dinoNameResponse);
-  });
-
-
-  app.get(`/dinoimage`, async(request,response) => {
-      const fetchApi = await fetch("https://bing-image-search1.p.rapidapi.com/images/search?q=Dinosaur&count=10", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": api_key,
-            "x-rapidapi-host": "bing-image-search1.p.rapidapi.com"
-        }
-    })
-    const dinoImageResponse = await fetchApi.json();
-    console.log(dinoImageResponse);
-    response.json(dinoImageResponse);
-})
